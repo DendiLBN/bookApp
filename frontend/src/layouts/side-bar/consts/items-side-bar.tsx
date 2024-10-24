53b1c1d106b4277ddc9a19e93c94228210bf3e2a
DendiLBN <damiansky1337@gmail.com>
2024-10-24 17:49:55 +0200

    fe implement user sidebar panel settings

--- frontend/src/layouts/side-bar/consts/items-side-bar.tsx
+++ frontend/src/layouts/side-bar/consts/items-side-bar.tsx
@@ -1,77 +1,30 @@
 import { MenuProps } from "antd";
 import {
-  MailOutlined,
-  DesktopOutlined,
-  PieChartOutlined,
-  ContainerOutlined,
-  AppstoreOutlined,
+  UserOutlined,
+  SettingOutlined,
+  KeyOutlined,
 } from "@ant-design/icons";
 
 export const itemsSideBar: MenuProps["items"] = [
   {
     key: "1",
-    icon: <PieChartOutlined />,
-    label: "Home",
+    icon: <UserOutlined />,
+    label: "Profile",
   },
   {
     key: "2",
-    icon: <DesktopOutlined />,
-    label: "About",
-  },
-  {
-    key: "3",
-    icon: <ContainerOutlined />,
-    label: "Pages",
-  },
-  {
-    key: "sub1",
-    label: "Navigation One",
-    icon: <MailOutlined />,
+    icon: <SettingOutlined />,
+    label: "Settings",
     children: [
       {
-        key: "5",
-        label: "Option 5",
+        key: "3",
+        label: "Change Password",
+        icon: <KeyOutlined />,
       },
       {
-        key: "6",
-        label: "Option 6",
-      },
-      {
-        key: "7",
-        label: "Option 7",
-      },
-      {
-        key: "8",
-        label: "Option 8",
-      },
-    ],
-  },
-  {
-    key: "sub2",
-    label: "Navigation Two",
-    icon: <AppstoreOutlined />,
-    children: [
-      {
-        key: "9",
-        label: "Option 9",
-      },
-      {
-        key: "10",
-        label: "Option 10",
-      },
-      {
-        key: "sub3",
-        label: "Submenu",
-        children: [
-          {
-            key: "11",
-            label: "Option 11",
-          },
-          {
-            key: "12",
-            label: "Option 12",
-          },
-        ],
+        key: "4",
+        icon: <UserOutlined />,
+        label: "Update Avatar",
       },
     ],
   },
